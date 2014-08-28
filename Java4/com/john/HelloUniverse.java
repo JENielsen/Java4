d8fa9d56a2eae1ecbc628a661011b536e41ad6c1
JENielsen <john.nielsen@helpsystems.com>
2014-08-28 12:07:14 -0500

    change 1 for release 2

--- Java4/com/john/HelloUniverse.java
+++ Java4/com/john/HelloUniverse.java
@@ -4,7 +4,7 @@ public class HelloUniverse {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        // change2 for release 1 branch
+        // change1 for release 2 branch
 	}
 
 }
