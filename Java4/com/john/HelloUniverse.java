6f3b888f9d61ccfc4e3ecfa562cdf09bf9b7a652
JENielsen <john.nielsen@helpsystems.com>
2014-08-28 15:34:16 -0500

    System.out commit

--- Java4/com/john/HelloUniverse.java
+++ Java4/com/john/HelloUniverse.java
@@ -5,6 +5,7 @@ public class HelloUniverse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         // change2 for release 2 branch
+		System.out.println("Hello Release 2 Universe");
 	}
 
 }
